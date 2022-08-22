9100a0a14d52c89641363537f8d35ee4f512691b
jeehye03 <ks6247@naver.com>
2022-08-22 22:58:17 +0900

    SSR

--- next.config.js
+++ next.config.js
@@ -1,7 +1,27 @@
 /** @type {import('next').NextConfig} */
+
+const API_KEY = "d9e24f03761ab7744b951061b064b7bd";
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  async redirects() {
+    return [
+      {
+        source: "/old-blog/:path*",
+        destination: "/new-blog/:path*",
+        permanent: false,
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/movies",
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,12 @@
+import Head from "next/head";
 import NavBar from "../components/NavBar";
 
 export default function Test() {
   return (
     <div>
-      <NavBar />
+      <Head>
+        <title>Home | Next Movies</title>
+      </Head>
       <h1>About</h1>
     </div>
   );
