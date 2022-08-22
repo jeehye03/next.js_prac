0ba1b1f7fef4c0b41559285f847a833e4fc4d079
jeehye03 <ks6247@naver.com>
2022-08-22 13:54:19 +0900

    키워드 - 기능, 단위 #issue번호
    변경사항 :

--- pages/about.js
+++ pages/about.js
@@ -1,3 +1,10 @@
+import NavBar from "../components/NavBar";
+
 export default function Test() {
-  return "about us";
+  return (
+    <div>
+      <NavBar />
+      <h1>About</h1>
+    </div>
+  );
 }
