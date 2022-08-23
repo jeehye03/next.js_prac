2ea950fd53629e0aea1427c3d54bbef006925a35
jeehye03 <ks6247@naver.com>
2022-08-23 15:50:50 +0900

    next.js

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,11 @@
 import Seo from "../components/Seo";
-import { useEffect, useState } from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
 
 export default function Home({ results }) {
   const router = useRouter();
   const onClick = (id, title) => {
-    router.push(
-      {
-        pathname: `/movies/${id}`,
-        query: {
-          title,
-        },
-      },
-      `/movies/${id}`
-    );
+    router.push(`/movies/${title}/${id}`);
   };
 
   return (
@@ -29,15 +20,7 @@ export default function Home({ results }) {
         >
           <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
           <h4>
-            <Link
-              href={{
-                pathname: `/movies/${movie.id}`,
-                query: {
-                  title: movie.original_title,
-                },
-              }}
-              as={`/movies/${movie.id}`}
-            >
+            <Link href={`/movies/${movie.original_title}/${movie.id}`}>
               <a>{movie.original_title}</a>
             </Link>
           </h4>
